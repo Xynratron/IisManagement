5a09c15b56fea1787cfc3312154ab362014e039c
Simon Dallmair <simon.dallmair@bmfgroup.de>
2018-04-13 14:25:26 +0200

    Added pictures Folder User

--- IisManagement.Server/ServerSettings.cs
+++ IisManagement.Server/ServerSettings.cs
@@ -1,11 +1,19 @@
-﻿namespace IisManagement.Server
+﻿using System.Security.Policy;
+using NLog;
+
+namespace IisManagement.Server
 {
     public class ServerSettings
     {
         public static int Port { get; set; }
 
         public static string BasePath { get; set; }
-
-       
+        
+        public static Pictures Pictures { get; set; }
+    }
+    public class Pictures
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
