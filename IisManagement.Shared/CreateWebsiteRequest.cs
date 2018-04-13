da3a4a36e9f2156f122df93b6c99e1b5c4dd25b4
Simon Dallmair <simon.dallmair@bmfgroup.de>
2018-04-13 14:20:24 +0200

    Added More Messages

--- IisManagement.Shared/CreateWebsiteRequest.cs
+++ IisManagement.Shared/CreateWebsiteRequest.cs
@@ -6,6 +6,21 @@ namespace IisManagement.Shared
 {
     public class CreateWebsiteRequest
     {
+        public IisSite SiteInformation { get; set; }
+    }
 
+    public class ReadServerContents
+    {
+        public List<IisSite> SiteInformations { get; set; }
+    }
+
+    public class IisSite
+    {
+        public string Group { get; set; }
+        public string SiteName { get; set; }
+        public List<string> Domains { get; set; }
+        public bool AddPictures { get; set; }
+        public bool LocalPictures { get; set; }
+        public string AppPoolName { get; set; }
     }
 }
